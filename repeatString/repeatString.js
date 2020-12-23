7542ef7130691e3497bd6e4075d8cb8ed2c120a9
githubofjk <hkim317@gmail.com>
2020-12-22 20:58:34 -0800

    helloWorld, repeatString, reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(input, counter) {
+    let result = '';
 
+    if (input === '') {
+        return result;
+    } else if (counter < 0) {
+        result = 'ERROR';
+        return result;
+    } else if (counter === 0) {
+        return result;
+    } else {
+        for (i = 0; i < counter; i++) {
+            result += input;
+        }
+    }
+    return result;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let result = '';
 
+    let array = input.split("");
+
+    let arraylength = array.length - 1;
+
+    for (i = arraylength; i >= 0; i--) {
+        result += array[i];
+    }
+
+    return result;
 }
 
 module.exports = reverseString
