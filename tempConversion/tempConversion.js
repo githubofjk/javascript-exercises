e0707c5e7f1cfd4e7929f4988cbab05898c6d141
githubofjk <hkim317@gmail.com>
2020-12-23 08:59:44 -0800

    Updated tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,26 @@
-const ftoc = function() {
+const ftoc = function(arg1) {
+    let result = (arg1 - 32) * (5 / 9);
+    result = parseFloat(result.toFixed(1));
 
+    if (result === 0.0) {
+        return 0;
+    }
+
+    return result;
 }
 
-const ctof = function() {
+const ctof = function(arg1) {
+    let result = arg1 * (9 / 5) + 32;
+    result = parseFloat(result.toFixed(1));
 
+    if (result === 0.0) {
+        return 0;
+    }
+
+    return result;
 }
 
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 }
