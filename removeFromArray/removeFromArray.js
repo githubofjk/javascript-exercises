fe7714f289c9f6b925287eca1172209e44a67cc8
githubofjk <hkim317@gmail.com>
2020-12-22 22:45:56 -0800

    Completed removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remainingArgs) {
+    let result = arguments[0];
+    let remainingArgsArray = remainingArgs;
 
+    let resultLength = result.length;
+    let remainingArgsArrayLength = remainingArgsArray.length;
+
+    for (i = 0; i < remainingArgsArrayLength; i++) {
+        for (j = 0; j < resultLength; j++) {
+            if (remainingArgsArray[i] === result[j]) {
+                result.splice(j, 1);
+            }
+        }
+    }
+
+    return result;
 }
 
 module.exports = removeFromArray
