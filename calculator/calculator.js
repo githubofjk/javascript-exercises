c43f31a3a469e242d079e4dfd8ac8dec4c3cd8e3
githubofjk <hkim317@gmail.com>
2020-12-29 15:21:07 -0800

    updated calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,54 @@
-function add () {
-	
+function add(a, b) {
+    return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+    return a - b;
 }
 
-function sum () {
-	
+function sum(param) {
+    let result = 0;
+
+    if (param.length === 0) {
+        return 0;
+    } else if (param.length === 1) {
+        return param[0];
+    } else {
+        param.forEach(value => result += value);
+        return result;
+    }
 }
 
-function multiply () {
-	
+function multiply(param) {
+    let result = 1;
+
+    param.forEach(value => result *= value);
+    return result;
 }
 
-function power() {
-	
+function power(a, b) {
+    let result = 1;
+
+    for (i = 0; i < b; i++) {
+        result *= a;
+    }
+
+    return result;
 }
 
-function factorial() {
-	
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
+    add,
+    subtract,
+    sum,
+    multiply,
     power,
-	factorial
+    factorial
 }
