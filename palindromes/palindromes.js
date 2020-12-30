a428775c460bc62972fb56d819c1342681aaf3ec
githubofjk <hkim317@gmail.com>
2020-12-29 16:56:06 -0800

    update palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let reverse = string.replace(/[ !,.]/g, "").split('').reverse().join('').toLowerCase();
+    return reverse === string.replace(/[ !,.]/g, "").toLowerCase();
 }
 
 module.exports = palindromes
