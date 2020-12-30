892e82736e031e426c88b3cdfca3754858f95035
githubofjk <hkim317@gmail.com>
2020-12-30 15:36:55 -0800

    update cipher calulation for nnegative modulo

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,35 +1,19 @@
 const caesar = function(string, key) {
-    let charUnicode;
-    let charUnicodeCipher;
-    let stringCipher = "";
+    return string
+        .split("")
+        .map(char => cipher(char, key))
+        .join("");
+}
 
-    // shift string characters using caesar's cipher key
+// shift string characters using caesar's cipher key
+function cipher(char, key) {
+    let charUnicode = char.charCodeAt();
+    let charCase = charUnicode > 'Z'.charCodeAt() ? 'a'.charCodeAt() : 'A'.charCodeAt();
+    let isPunctuation = (charUnicode >= 32 && charUnicode <= 47) ? true : false;
+
     // Caesar's cipher = (character + key) % 26
-    for (i = 0; i < string.length; i++) {
-        charUnicode = string.charCodeAt(i);
-        // keep punctuation and spaces
-        if (charUnicode >= 32 && charUnicode <= 47) {
-            stringCipher += String.fromCharCode(charUnicode);
-        }
-        // preserve upper and lower case 
-        else if (charUnicode >= 65 && charUnicode <= 90) {
-            charUnicodeCipher = (((charUnicode - 'A'.charCodeAt()) + key) % 26) + 'A'.charCodeAt();
-            // works with negative shift
-            if (charUnicodeCipher < 65) {
-                charUnicodeCipher += 26;
-            }
-            stringCipher += String.fromCharCode(charUnicodeCipher);
-        } else if (charUnicode >= 97 && charUnicode <= 122) {
-            charUnicodeCipher = (((charUnicode - 'a'.charCodeAt()) + key) % 26) + 'a'.charCodeAt();
-            // works with negative shift
-            if (charUnicodeCipher < 97) {
-                charUnicodeCipher += 26;
-            }
-            stringCipher += String.fromCharCode(charUnicodeCipher);
-        }
-    }
-
-    return stringCipher;
+    let cipherChar = isPunctuation ? char : String.fromCharCode(((((charUnicode + key - charCase) % 26) + 26) % 26) + charCase);
+    return cipherChar
 }
 
 module.exports = caesar
