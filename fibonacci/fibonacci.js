03dfed40acc59d77d2489b09491aee0e5123107d
githubofjk <hkim317@gmail.com>
2020-12-30 16:20:19 -0800

    update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // doesn't accept negatives
+    if (n < 0) {
+        return "OOPS";
+    }
+    // DOES accept strings
+    n = parseInt(n);
 
+    // https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
+    if (n <= 1) {
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci
