ecc86b0b158be3f42710eef2323dfd674d2bbc51
githubofjk <hkim317@gmail.com>
2020-12-30 22:46:41 -0800

    updated findTheOldest, passes all specs

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,19 +1,27 @@
 let findTheOldest = function(people) {
     return people.reduce((oldest, person) => {
-        // if oldestPerson object is empty, then current person is oldest
+        // if oldest object is empty, then current person is oldest
         if (("name" in oldest) === false) {
             for (key in person) {
                 oldest[key] = person[key];
             }
         }
 
-        // replace existing oldest if current person is older than existing oldes
-        if ((person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)) {
+        let date = new Date();
+        currentYear = date.getFullYear();
+
+        // if oldest has no death-date, set to this year
+        let oldestYearOfDeath = oldest.yearOfDeath ? oldest.yearOfDeath : currentYear;
+
+        // if person with no death-date, get their age as of today
+        let personYearOfDeath = person.yearOfDeath ? person.yearOfDeath : currentYear;
+
+        // replace existing oldest if current person is older than existing oldest
+        if ((personYearOfDeath - person.yearOfBirth) > (oldestYearOfDeath - oldest.yearOfBirth)) {
             for (key in person) {
                 oldest[key] = person[key];
             }
         }
-
         return oldest;
     }, {});
 }
